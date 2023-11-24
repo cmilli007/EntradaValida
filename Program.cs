65d96ceaf528635e6093b63b45df8537a8a4a72b
camili <camili.costa07@gmail.com>
2023-11-24 20:03:57 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Entrada Válida\n");
+ Console.ResetColor();
+int numeroDigitado = -1;
+ 
+while (numeroDigitado < 0 || numeroDigitado > 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para sair): ");
+    numeroDigitado = Convert.ToInt32(Console.ReadLine());
+}
+ 
+Console.WriteLine();
+ 
+if (numeroDigitado == 0)
+{
+    Console.WriteLine("Operação cancelada.\n");
+}
+ 
+else {
+    Console.WriteLine($"Número selecionado = {numeroDigitado}.");
+}
+ 
+Console.WriteLine("Volte sempre!\n");
